68b2e17db3ce03f87060908993ec43e764431d20
Petar Slavnic <petarslavnic@gmail.com>
2020-01-08 08:55:01 +0100

    Update test and make first snapshot

--- test/useDragScroll.spec.js
+++ test/useDragScroll.spec.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { mount } from 'enzyme'
+import useDragScroll from '../src'
+
+const HookWrapper = () => {
+  const ref = useDragScroll()
+  return <div ref={ref} />
+}
 
 describe('useDragScroll', () => {
+  let wrapper
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
   it('should do something', () => {
-    expect(true).toBeTruthy()
+    wrapper = mount(<HookWrapper />)
+
+    wrapper.find('div').first().simulate('dragover')
+
+    expect(wrapper).toMatchSnapshot()
   })
 })
